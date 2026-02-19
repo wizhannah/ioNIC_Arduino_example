71ea21447458a6abef181c56ddd0ed41c5b730c9
wizhannah <hannah@wiznet.io>
2026-02-19 11:20:34 +0900

    Add AWS MQTTS example

---
+++ mqtts/aws_mqtts_spi/certification.h
@@ -0,0 +1,23 @@
+#pragma once
+
+static const char *ROOTCA =
+"-----BEGIN CERTIFICATE-----\r\n"
+"...\r\n"
+"...\r\n"
+"...\r\n"
+"-----END CERTIFICATE-----";
+
+
+static const char *CLIENT_CERT =
+"-----BEGIN CERTIFICATE-----\r\n"
+"...\r\n"
+"...\r\n"
+"...\r\n"
+"-----END CERTIFICATE-----";
+
+static const char *PRIVATE_KEY =
+"-----BEGIN RSA PRIVATE KEY-----\r\n"
+"...\r\n"
+"...\r\n"
+"...\r\n"
+"-----END RSA PRIVATE KEY-----";
